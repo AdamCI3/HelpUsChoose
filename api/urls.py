f87bc30085d11d34d44cc68d9d7f90f07e789b8f
Krzysztof Borowski <hisboak@gmail.com>
2024-01-09 01:18:17 +0100

    Added next item api

--- api/urls.py
+++ api/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path("init", views.init, name="init"),
     path("session_check", views.session_check, name="session_check"),
     path(r'new_game/<int:category_id>', views.new_game, name='new game'),
-    path(r'join/<int:game_id>', views.join)
+    path(r'join/<int:game_id>', views.join),
+    path(r'next/<int:game_id>/<str:move>', views.next)
 ]
--- api/views.py
+++ api/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 import random
+import json
 
 from api.models import User, Game, Item
 from mainpage.models import Category
@@ -30,11 +31,11 @@ def new_game(request, category_id):
     category_list = list(Category.objects.filter(id=category_id)) 
     if len(category_list) <= 0: 
         return HttpResponse("ERR")
-    category = Category.objects.get(id=category_id)
+    category = Category.objects.get(id=category_id) # there are no categories with this category_id
     
     items_list = list(Item.objects.filter(category_id=category)) 
     if len(items_list) <= 0: 
-        return HttpResponse("ERR")
+        return HttpResponse("ERR") # there are no items in this category
     
     new_game = Game.objects.create(
         url="", 
@@ -78,15 +79,60 @@ def join(request, game_id):
 
     return HttpResponse("OK")
 
-def next(request, move):
+def next(request, game_id, move):
     # This function should return next move
     # Every time this api enpoint is called it
     # should run logic to calculate matches
-    # 
-    # ARGUMENT: 
-    # 0 - Left
-    # 1 - Right
+    #
+    # ARGUMENT game_id: 
+    # int - game_id
+    #
+    # ARGUMENT move: 
+    # str - left - Left
+    # str - right - Right
     #
     # RETURN:
     # INSTANCE OF ITEM
-    pass
+    
+    if 'id' not in request.session:
+        return HttpResponse("ERR1")
+    current_user = User.objects.get(id=request.session['id'])
+    
+    game_list = list(Game.objects.filter(id=game_id)) 
+    if len(game_list) <= 0: 
+        return HttpResponse("ERR2")
+    game = Game.objects.get(id=game_id) # there are no games with this game_id
+    
+    if game.owner_id != current_user and game.player_id != current_user:
+        return HttpResponse("ERR3") # user has no permission to make moves in this game
+    
+    category_id = game.category_id.id
+    category_list = list(Category.objects.filter(id=category_id)) 
+    if len(category_list) <= 0: 
+        return HttpResponse("ERR4")
+    category = Category.objects.get(id=category_id) # there are no categories with this category_id
+    
+    items_list = list(Item.objects.filter(category_id=category)) 
+    if len(items_list) <= 0: 
+        return HttpResponse("ERR5") # there are no items in this category
+    
+    next_item: Item = {}
+    print(request.session['queues'])
+    if len(request.session['queues'][str(game_id)]) != 0:
+        next_item_id = request.session['queues'][str(game_id)][0]
+        request.session['queues'][str(game_id)].pop(0)
+        next_item = Item.objects.get(id=next_item_id)
+    else: 
+        next_item = random.choice(items_list)
+    
+    # TO DO:
+    # Change next item if move == 'right'
+    
+    request.session.modified = True
+    
+    response = {
+        "id": next_item.id,
+        "name": next_item.name
+    }
+    print(response)
+    return HttpResponse(json.dumps(response))
